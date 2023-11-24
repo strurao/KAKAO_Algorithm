7747d2abb0fb7d43518a03a101e39bb1c0cb0c34
strurao <strurao@gmail.com>
2023-11-24 21:22:11 +0900

    합승 택시 요금

--- KAKAO_Algorithm/2021 BLIND 04.cpp
+++ KAKAO_Algorithm/2021 BLIND 04.cpp
@@ -22,10 +22,47 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
 }
 
 
+////////////////////////////////////////////////
+
+#include <bits/stdc++.h>
+using namespace std;
+
 // 다익스트라
-/*
+using pii = pair<int, int>;
+int d[3][205];
+int INF = 1e8 + 10;
+vector<pii> adj[205];
+
+void dijkstra(int st, int idx) {
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    fill(d[idx], d[idx] + 201, INF);
+    d[idx][st] = 0;
+    pq.push({ d[idx][st], st });
+    while (!pq.empty()) {
+        auto cur = pq.top();
+        pq.pop();
+        int dist = cur.first, i = cur.second;
+        if (d[idx][i] != dist) continue;
+        for (auto nxt : adj[i]) {
+            int cost = nxt.first, ni = nxt.second;
+            if (d[idx][ni] > dist + cost) {
+                d[idx][ni] = dist + cost;
+                pq.push({ d[idx][ni], ni });
+            }
+        }
+    }
+}
+
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    for (auto v : fares) {
+        adj[v[0]].push_back({ v[2], v[1] });
+        adj[v[1]].push_back({ v[2], v[0] });
+    }
+    dijkstra(s, 0);
+    dijkstra(a, 1);
+    dijkstra(b, 2);
     int answer = INF;
+    for (int mid = 1; mid <= n; mid++)
+        answer = min(answer, d[0][mid] + d[1][mid] + d[2][mid]);
     return answer;
 }
-*/
